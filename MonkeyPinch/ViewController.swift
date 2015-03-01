817aa751de9c8d0f814cd411294a641b233496ce
steelers11 <michaelpraska@icloud.com>
2015-02-28 18:12:25 -0700

    UIGestures foolery.

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -21,5 +21,35 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x, y:recognizer.view!.center.y + translation.y)
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+        
+        if recognizer.state == UIGestureRecognizerState.Ended {
+            // 1
+            let velocity = recognizer.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            println("magnitude: \(magnitude), slide multiplier: \(slideMultiplier)")
+            
+            // 2
+            let slideFactor = 0.1 * slideMultiplier
+            // 3
+            var finalPoint = CGPoint(x: recognizer.view!.center.x + (velocity.x * slideFactor),
+                y: recognizer.view!.center.y + (velocity.y * slideFactor))
+            
+            // 4
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            // 5 
+            UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut,
+                animations: {recognizer.view!.center = finalPoint},
+                completion: nil)
+            
+        }
+    }
+}
