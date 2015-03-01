8239fcfb7c3477c6210ee5774b555514b128aadb
steelers11 <michaelpraska@icloud.com>
2015-03-01 07:41:25 -0700

    Added comments to asist understanding

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -20,32 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-
-
     @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        // translation is to track how long the user holds their finger on the object
         let translation = recognizer.translationInView(self.view)
+        // the 'view!.center' refernces to the image and its bounds
         recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x, y:recognizer.view!.center.y + translation.y)
+        // you need this line to stop the gesture from moving any longer
         recognizer.setTranslation(CGPointZero, inView: self.view)
         
+        // everything after this line is for the sliding effect
         if recognizer.state == UIGestureRecognizerState.Ended {
-            // 1
+    
             let velocity = recognizer.velocityInView(self.view)
             let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
             let slideMultiplier = magnitude / 200
             println("magnitude: \(magnitude), slide multiplier: \(slideMultiplier)")
             
-            // 2
             let slideFactor = 0.1 * slideMultiplier
-            // 3
+           
             var finalPoint = CGPoint(x: recognizer.view!.center.x + (velocity.x * slideFactor),
                 y: recognizer.view!.center.y + (velocity.y * slideFactor))
             
-            // 4
             finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
             finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
             
-            // 5 
             UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut,
                 animations: {recognizer.view!.center = finalPoint},
                 completion: nil)
